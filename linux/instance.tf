435a338c3c77e7eafbac4b609f929659f8e09aa7
alfonsoh <admin@administrator.attlocal.net>
2021-07-24 15:05:12 -0700

    adding key so you can login to instance

--- linux/instance.tf
+++ linux/instance.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "DEVOPS" {
   ami           = var.AMIS[var.AWS_REGION]
   instance_type = "t2.micro"
+  key_name = "pem_file_name"
 tags = {
-        Name = "YOUR NAME Terrafrom Test"
+        Name = "DevOps Terrafrom Test"
     }
 
 }
